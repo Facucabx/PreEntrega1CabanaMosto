7fa58305daec71ef26b8fbc7b675e8760f2455f0
Facu <facu.zero@gmail.com>
2023-07-10 01:05:26 -0300

    correcciones de formato

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ var jugadorFavorito = prompt("Eligí tu jugador favorito de la alineación: Mart
 
 // Jugador elegido y su información sobre él
 if (jugadorFavorito === "Messi") {
-  console.log("Has elegido a Lionel Messi como tu jugador favorito. ¡Anota goles y lidera el ataque de la selección!");
+    console.log("Has elegido a Lionel Messi como tu jugador favorito. ¡Anota goles y lidera el ataque de la selección!");
 } else if (jugadorFavorito === "Alvarez") {
     console.log("Has elegido a la araña como tu jugador favorito. ¡Anota goles y crea oportunidades de gol!");
 } else if (jugadorFavorito === "Di Maria") {
@@ -15,11 +15,11 @@ if (jugadorFavorito === "Messi") {
 } else if (jugadorFavorito === "Fernandez") {
     console.log("Has elegido a Enzo Fernandez como tu jugador favorito. ¡Controla el centro del campo y crea oportunidades de gol!");
 } else if (jugadorFavorito === "Paredes") {
-  console.log("Has elegido a Leandro Paredes como tu jugador favorito. ¡Controla el centro del campo y crea oportunidades de gol!");
+    console.log("Has elegido a Leandro Paredes como tu jugador favorito. ¡Controla el centro del campo y crea oportunidades de gol!");
 } else if (jugadorFavorito === "De Paul") {
     console.log("Has elegido a Rodri De Paul como tu jugador favorito. ¡Controla el centro del campo y crea oportunidades de gol!");
 } else if (jugadorFavorito === "Otamendi") {
-  console.log("Has elegido a Nicolás Otamendi como tu jugador favorito. ¡Defiende con solidez y evita los ataques del equipo contrario!");
+    console.log("Has elegido a Nicolás Otamendi como tu jugador favorito. ¡Defiende con solidez y evita los ataques del equipo contrario!");
 } else if (jugadorFavorito === "Tagliafico") {
     console.log("Has elegido a Tagliafico como tu jugador favorito. ¡Defiende con solidez y evita los ataques del equipo contrario!");
 } else if (jugadorFavorito === "Molina") {
@@ -27,10 +27,10 @@ if (jugadorFavorito === "Messi") {
 } else if (jugadorFavorito === "Romero") {
     console.log("Has elegido a Romero como tu jugador favorito. ¡Defiende con solidez y evita los ataques del equipo contrario!");
 } else if (jugadorFavorito === "Martinez") {
-  console.log("Has elegido a Emiliano Martínez como tu jugador favorito. ¡Sé el último obstáculo y realiza grandes paradas para mantener el arco seguro!");
+    console.log("Has elegido a Emiliano Martínez como tu jugador favorito. ¡Sé el último obstáculo y realiza grandes paradas para mantener el arco seguro!");
 } else {
-  console.log("Jugador no reconocido. Comenzaremos la simulación con Lionel Messi como tu jugador favorito.");
-  jugadorFavorito = "Messi";
+    console.log("Jugador no reconocido. Comenzaremos la simulación con Lionel Messi como tu jugador favorito.");
+    jugadorFavorito = "Messi";
 }
 
 // Alineación de la selección
@@ -41,12 +41,12 @@ var alineacion = ["Martinez", "Molina", "Romero", "Otamendi", "Tagliafico", "De 
 
 // Mostrar los jugadores
 for (var i = 0; i < alineacion.length; i++) {
-  var jugador = alineacion[i];
-  if (jugador === jugadorFavorito) {
-    console.log("- ¡" + jugador + " (Tu jugador favorito)!");
-  } else {
-    console.log("- " + jugador);
-  }
+    var jugador = alineacion[i];
+    if (jugador === jugadorFavorito) {
+        console.log("- ¡" + jugador + " (Tu jugador favorito)!");
+    } else {
+        console.log("- " + jugador);
+    }
 }
 
 // Mensaje de celebración de la tercera estrella
